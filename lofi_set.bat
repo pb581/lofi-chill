a853658d45977df44dbe7c8c69d6b48b17c0866f
Prasun Biswas <prasunbiswas.2012@gmail.com>
2022-01-10 04:25:21 +0530

    run lofi_set.bat and chill
    added logger

--- lofi_set.bat
+++ lofi_set.bat
@@ -3,5 +3,4 @@ if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && 
 cd /D %~dp0
 
 call renew_network_config.bat
-python yt_stream.py
-call lofi.bat
+python yt_stream.py >log.txt && call lofi.bat
