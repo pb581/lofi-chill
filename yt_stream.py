8f9d6fa3e0f6ad162e5768e9b190be23aa2dad20
Prasun Biswas <prasunbiswas.2012@gmail.com>
2022-01-10 03:57:41 +0530

    fetch playurl

--- yt_stream.py
+++ yt_stream.py
@@ -1,14 +1,17 @@
 from pafy import new
 #from pyperclip import copy
+print("Required modules loaded!\n")
 
 url = "https://www.youtube.com/watch?v=5qap5aO4i9A"
 video = new(url)
 best = video.getbest()
 playurl = best.url	#network stream url to play in vlc
 
+print("Fresh MRL fetched: ", playurl, sep='',end='\n\n')
 #copy(playurl)			 #copy fetched url to clipboard
 
 #dump the last fetched playurl to a txt file in desktop in case it is lost from clipboard
 file = open(r"lofi_last_fetch.txt",'w')
 file.write(playurl)
+print("MRL written to lofi_last_fetch.txt")
 file.close()
